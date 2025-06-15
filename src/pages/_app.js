f5a2900f31640dc9cc391ce55ae2144bd87c14ce
Yuvasri-10 <71762307062@cit.edu.in>
2025-06-15 08:29:26 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,14 @@
-import '../styles/index.css'; 
+import '../styles/index.css';
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react'; // ✅ Add this line
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics /> {/* ✅ Add this line */}
+    </>
+  );
 }
 
 export default MyApp;
